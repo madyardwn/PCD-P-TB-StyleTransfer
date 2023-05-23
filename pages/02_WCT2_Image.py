f1539ba018c3e5f3b2ae9d7d9039d23014067ca8
FahmiX <fahmiahmadfadilah7@gmail.com>
2023-05-23 12:11:17 +0700

    Fix beberapa error

--- pages/02_WCT2_Image.py
+++ pages/02_WCT2_Image.py
@@ -4,6 +4,8 @@ import functions as func
 
 # Tampilan aplikasi Streamlit
 def main():
+    image_directory = "images"
+
     st.title("Implementasi WCT2 pada Video")
     st.write("Unggah citra konten dan style untuk melakukan style transfer.")
 
@@ -20,10 +22,10 @@ def main():
         st.image(style_file, width=1024)
 
         # Mengatur path untuk menyimpan citra konten yang baru diupload
-        uploaded_content = os.path.join("images", "content.png")
+        uploaded_content = os.path.join(image_directory, "content.png")
 
         # Mengatur path untuk menyimpan citra style yang baru diupload
-        uploaded_style = os.path.join("images", "style.png")
+        uploaded_style = os.path.join(image_directory, "style.png")
 
         # Menghapus citra konten yang diunggah jika sudah ada
         if os.path.exists(uploaded_content):
@@ -46,7 +48,7 @@ def main():
             st.write("Memulai proses implementasi WCT2 pada citra...")
 
             # Perform Style Transfer
-            func.perform_style_transfer(uploaded_content, uploaded_style, "images")
+            func.perform_single_style_transfer(uploaded_content, uploaded_style, "images")
 
 # Menjalankan aplikasi
 if __name__ == "__main__":
